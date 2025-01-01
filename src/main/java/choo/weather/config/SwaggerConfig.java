ecaae8738b1cc9f30844d188207733b40fdd33d3
HyunJooong <hjchoo19950327@gmail.com>
2025-01-02 00:19:40 +0900

    swagger implement

--- src/main/java/choo/weather/config/SwaggerConfig.java
+++ src/main/java/choo/weather/config/SwaggerConfig.java
@@ -1,33 +1,25 @@
 package choo.weather.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build().apiInfo(apiInfo());
+    public OpenAPI api() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    private Info apiInfo() {
+        return new Info()
                 .title("title")
                 .description("description")
-                .version("1.0")
-                .build();
+                .version("1.0.0");
     }
 
 }
